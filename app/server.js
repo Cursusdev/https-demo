aeac384a87de35bd84dfce30ffd1da26af9140b2
httpcode <yannick806@gmail.com>
2020-09-01 16:26:37 +0200

    commit11

--- app/server.js
+++ app/server.js
@@ -10,4 +10,9 @@ app.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000);
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
